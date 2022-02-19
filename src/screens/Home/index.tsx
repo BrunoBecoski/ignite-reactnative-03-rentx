4aead8cfe1949624d7e3895d660469f16d763ea3
Bruno Becoski <brunobecoski@gmail.com>
2022-02-18 22:29:39 -0300

    Criação da Home e do Card Car

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,16 +1,68 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+import Logo from '../../assets/logo.svg';
+
+import { Car } from '../../components/Car';
 
 import {
   Container,
-  Title
+  Header,
+  HeaderContent,
+  TotalCars
 } from './styles';
 
 export function Home(){
+  const carDataOne = {
+    brand: 'Audi',
+    name: 'RS 5 Coupé',
+    rent: {
+      pediod: 'AO DIA',
+      price: 120,
+    },
+    type: 'energy',
+    thumbnail: 'https://freepngimg.com/thumb/audi/35227-5-audi-rs5-red.png'
+  }
+
+  const carDataTwo = {
+    brand: 'Porsche',
+    name: 'Panamera',
+    rent: {
+      pediod: 'AO DIA',
+      price: 550,
+    },
+    type: 'gasoline',
+    thumbnail: 'https://www.pngkit.com/png/full/237-2375888_porsche-panamera-s.png'
+  }
+
+
   return (
     <Container>
-      <Title>
-        HOME
-      </Title>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Header>
+        <HeaderContent>
+          <Logo 
+            width={RFValue(108)}
+            height={RFValue(12)}
+          />
+          <TotalCars>
+            Total de 12 carros
+          </TotalCars>
+        </HeaderContent>
+      </Header>
+
+      <Car 
+        data={carDataOne}
+      />      
+      <Car 
+        data={carDataTwo}
+      />
+
     </Container>
   );
 }
