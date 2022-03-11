75ccbc3d18ca3dd93001652d7690d3dab3243aac
Bruno Becoski <brunobecoski@gmail.com>
2022-03-10 23:48:43 -0300

    Sincronização

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,19 +2,23 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 
 import { useAuth } from '../hooks/auth';
-
 import { AppTabRoutes } from './app.tab.routes';
 import { AuthRoutes } from './auth.routes';
+
+import { LoadAnimation } from '../components/LoadAnimation';
  
 export function Routes(){
-  const { user } = useAuth();
+  const { user, loading } = useAuth();
 
-  return (
-    <NavigationContainer>
-      { user.id 
-        ? <AppTabRoutes /> 
-        : <AuthRoutes />
-      }
-    </NavigationContainer>
+  return (  
+    // loading 
+      // ? <LoadAnimation /> 
+      // : <NavigationContainer>
+      <NavigationContainer>
+        { user.id 
+          ? <AppTabRoutes /> 
+          : <AuthRoutes />
+        }
+      </NavigationContainer>
   );
 }
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,9 +2,13 @@ import React, { useEffect, useState } from 'react';
 import { StatusBar } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { RFValue } from 'react-native-responsive-fontsize';
+import { useNetInfo } from '@react-native-community/netinfo';
+import { synchronize } from '@nozbe/watermelondb/sync';
 
+import { database } from '../../database';
 import { api } from '../../services/api';
 import { CarDTO } from '../../dtos/CarDTO';
+import { Car as ModelCar } from '../../database/model/Car';
 
 import { Car } from '../../components/Car';
 import { LoadAnimation } from '../../components/LoadAnimation';
@@ -20,13 +24,31 @@ import {
 } from './styles';
 
 export function Home() {
-  const [cars, setCars] = useState<CarDTO[]>([]);
+  const [cars, setCars] = useState<ModelCar[]>([]);
   const [loading, setLoading] = useState(true);
 
+  const netInfo = useNetInfo();
   const navigation = useNavigation();
 
   function handleCarDetails(car: CarDTO) {
     navigation.navigate('CarDetails', { car });
+  }
+
+  async function offlineSynchronize() {
+    await synchronize({
+      database,
+      pullChanges: async ({ lastPulledAt }) => {
+        const response = await api
+          .get(`cars/sync/pull?lastPulledVersion=${lastPulledAt || 0}`);
+        
+        const { changes, lastestVersion } = response.data;
+        return { changes, timestamp: lastestVersion }
+      },
+      pushChanges: async ({ changes }) => {
+        const user = changes.user;
+        await api.post('/users/sync', user);
+      }
+    });
   }
 
   useEffect(() => {
@@ -34,9 +56,11 @@ export function Home() {
 
     async function fetchCars() {
       try {
-        const response = await api.get('/cars');
+        const carCollection = await database.get<ModelCar>('cars');
+        const cars = await carCollection.query().fetch();
+        
         if(isMounted) {
-          setCars(response.data);
+          setCars(cars);
         }
       } catch (error) {
         console.log(error);
@@ -52,6 +76,12 @@ export function Home() {
       isMounted = false;
     };
   }, []);
+
+  useEffect(() => {
+    if(netInfo.isConnected) {
+      offlineSynchronize();
+    }
+  }, [netInfo.isConnected])
 
   return (
     <Container>
